b85f5d0e6679a21c8fb22001e4bf26472c638c01
Michal Zavadil <misa.zavadil@gmail.com>
2022-02-23 21:32:14 +0100

    Added React and typescript

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>OPM Editor</header>
+    </div>
+  );
+}
+
+export default App;
